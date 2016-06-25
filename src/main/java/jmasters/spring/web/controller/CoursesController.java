35da02489808533539695c98279c96095c49d49a
Daniel Las <lasdaniel@gmail.com>
2016-06-25 14:09:27 +0200

    Fixes

--- src/main/java/jmasters/spring/web/controller/CoursesController.java
+++ src/main/java/jmasters/spring/web/controller/CoursesController.java
@@ -1,11 +1,14 @@
-package jmasters.spring.model;
+package jmasters.spring.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import jmasters.spring.entity.Course;
-import jmasters.spring.web.controller.RestControllerBase;
+import jmasters.spring.model.CoursesService;
+import jmasters.spring.model.RestService;
 
+@RestController
 @RequestMapping("/courses")
 public class CoursesController extends RestControllerBase<Course> {
 
--- src/main/java/jmasters/spring/web/controller/RestControllerBase.java
+++ src/main/java/jmasters/spring/web/controller/RestControllerBase.java
@@ -6,11 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import jmasters.spring.model.RestService;
 
-@RestController
 public abstract class RestControllerBase<T> {
 
     protected abstract RestService<T> service();
